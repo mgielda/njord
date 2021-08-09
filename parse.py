2a60b1e42296526e0f4fcb5b472dde3c800549c0
Michael Gielda <mgielda@antmicro.com>
2021-08-09 23:47:06 +0200

    Add categorization and naming

--- parse.py
+++ parse.py
@@ -5,6 +5,7 @@ import numpy as np
 from datetime import datetime
 import glob
 import yaml
+import csv
 
 data_filenames = glob.glob('./history/*.csv')
 df = pd.concat([pd.read_csv(dfn) for dfn in data_filenames])
@@ -22,7 +23,21 @@ field_names = bank_config['field_names']
 field_names = {field_names[k]:k for k in field_names}
 
 df.rename(columns=field_names, inplace=True)
+df.fillna('', inplace=True)
+
+if bank_config.get('unnamed_are_desc', False):
+    for column in df.filter(regex="Unname"):
+        df['description'] += " " + df[column]
 
 pd.set_option('max_columns', None)
 print(df[0:10])
 
+df['category'] = ''
+df['name'] = ''
+with open('assignments.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        df.loc[df['description'].str.contains(row[0]), 'name'] = row[1]
+        df.loc[df['description'].str.contains(row[0]), 'category'] = row[2]
+
+print(df[0:10])
